31fdb9e3fdc92384e08447c7575743cdbfb78f93
getEldarK <elderyendex@yandex.ru>
2023-10-14 13:37:39 +0500

    ПР 12 save progress

--- src/components/InfoTooltip.jsx
+++ src/components/InfoTooltip.jsx
@@ -1,6 +1,6 @@
 export default function InfoTooltip(props) {
     return (
-      <div className={`popup ${props.isOpen ? 'popup_opened' : ''}`} onClick={props.onCloseClick}>
+      <div className={`popup ${props.isOpen ? 'popup_is-opened' : ""}`} onClick={props.onCloseClick}>
         <div className="popup__info">
           <img className="popup__status" src={props.image} alt={props.title}/>
           <h2 className="popup__message">{props.title}</h2>
