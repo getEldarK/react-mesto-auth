b246b77ae9bf7f03d73aa68087490ca46bb0c930
getEldarK <elderyendex@yandex.ru>
2023-10-14 14:50:43 +0500

    после ревью добавил атрибут value в инпуты

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -22,8 +22,8 @@ export default function Login(props) {
         <section className="login">
             <h2 className="login__title">Вход</h2>
             <form className="login__form" onSubmit={handleSubmit}>
-                <input className="login__input" type="email" placeholder="Email" onChange={handleEmailInput} required/>
-                <input className="login__input" type="password" placeholder="Пароль" autoComplete="on" onChange={handlePasswordInput} required/>
+                <input className="login__input" type="email" placeholder="Email"  value={email} onChange={handleEmailInput} required/>
+                <input className="login__input" type="password" placeholder="Пароль" autoComplete="on" value={password} onChange={handlePasswordInput} required/>
                 <button className="login__btn" type="submit">Войти</button>
             </form>
         </section>
